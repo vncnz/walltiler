d284a0b4a5f51e53b0a0a44b3fb60469db6669b4
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2025-03-06 12:31:26 +0100

    Improvements, network call, new elements mapped

--- main.py
+++ main.py
@@ -1,4 +1,55 @@
 import pygame
+from urllib.request import urlopen
+import json
+
+lat = 0
+lon = 0
+url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,apparent_temperature,is_day,precipitation,rain,weather_code,relative_humidity_2m,wind_speed_10m&timezone=auto&forecast_days=1&daily=sunrise,sunset,daylight_duration"
+
+# with urlopen(url) as f:
+#     data = json.load(f)
+data = {
+    'latitude': 0, 
+    'longitude': 0, 
+    'generationtime_ms': 0.07557868957519531, 
+    'utc_offset_seconds': 3600, 
+    'timezone': 'Europe/Rome', 
+    'timezone_abbreviation': 'GMT+1', 
+    'elevation': 0, 
+    'current_units': {'time': 'iso8601', 'interval': 'seconds', 'temperature_2m': '°C', 'apparent_temperature': '°C', 'is_day': '', 'precipitation': 'mm', 'rain': 'mm', 'weather_code': 'wmo code', 'relative_humidity_2m': '%'}, 
+    'current': {
+        'time': '2025-03-06T11:30', 
+        'interval': 900, 
+        'temperature_2m': 16.2, 
+        'apparent_temperature': 7.3, 
+        'is_day': 1, 
+        'precipitation': 0.0, 
+        'rain': 0.0, 
+        'weather_code': 0, 
+        'relative_humidity_2m': 41,
+        'wind_speed_10m': 30
+    }, 
+    'daily_units': {'time': 'iso8601', 'sunrise': 'iso8601', 'sunset': 'iso8601', 'daylight_duration': 's'}, 
+    'daily': {
+        'time': ['2025-03-06'], 
+        'sunrise': ['2025-03-06T06:00'], 
+        'sunset': ['2025-03-06T18:00'], 
+        'daylight_duration': [40000.00]
+    }
+}
+
+is_windy = data['current']['wind_speed_10m']
+if is_windy < 2: is_windy = 0
+is_hot = data['current']['apparent_temperature'] > 29.9
+is_day = data['current']['is_day'] == 1
+
+mode = "normal"
+if data['current']['apparent_temperature'] > 25: mode = 'sun'
+if data['current']['apparent_temperature'] > 30: mode = 'hot'
+if data['current']['apparent_temperature'] < 5: mode = 'cold'
+
+# exit()
+
 
 # Inizializza Pygame
 pygame.init()
@@ -81,35 +132,53 @@ for tp, y in areas_y.items():
         "tz2": (6, 0, 1, 1),
         "tz3": (6, 1, 1, 1),
         "tz4": (5, 1, 1, 1),
+        "obj1": (12, 1, 3, 2),
+        "obj2": (15, 4, 3, 2),
+        "obj3": (9, 1, 3, 3),
     }
     for w,c in terrains.items():
         tile_map[f'{tp}_{w}'] = (c[0], c[1] + y, c[2], c[3])
 
-mode = "cold"
 to_draw = [
-    { "tile": f"{mode}_tt1", "pos": (10, 20) },
-    { "tile": f"{mode}_tt2", "pos": (11, 20) },
-    { "tile": f"{mode}_tt1", "pos": (12, 20) },
-    { "tile": f"{mode}_tt3", "pos": (13, 20) },
-    { "tile": f"{mode}_tt3", "pos": (14, 20) },
-    { "tile": f"{mode}_ta2", "pos": (15, 20) },
-    { "tile": f"{mode}_tr1", "pos": (15, 21) },
-    { "tile": f"{mode}_tz4", "pos": (15, 22) },
+    { "tile": f"{mode}_tt1", "pos": (4, 20) },
+    { "tile": f"{mode}_tt2", "pos": (5, 20) },
+    { "tile": f"{mode}_tt1", "pos": (6, 20) },
+    { "tile": f"{mode}_tt3", "pos": (7, 20) },
+    { "tile": f"{mode}_tt3", "pos": (8, 20) },
+    { "tile": f"{mode}_ta2", "pos": (9, 20) },
+    { "tile": f"{mode}_tr1", "pos": (9, 21) },
+    { "tile": f"{mode}_tz4", "pos": (9, 22) },
+    { "tile": f"{mode}_tt1", "pos": (10, 22) },
+    { "tile": f"{mode}_tt2", "pos": (11, 22) },
+    { "tile": f"{mode}_tt1", "pos": (12, 22) },
+    { "tile": f"{mode}_tt3", "pos": (13, 22) },
+    { "tile": f"{mode}_tt2", "pos": (14, 22) },
+    { "tile": f"{mode}_tt1", "pos": (15, 22) },
+    { "tile": f"{mode}_tt3", "pos": (16, 22) },
+    { "tile": f"{mode}_obj1", "pos": (6, 18) },
+    { "tile": f"{mode}_obj2", "pos": (12, 20) },
+    { "tile": f"{mode}_obj3", "pos": (15, 20) },
 ]
-
-is_windy = True
-is_hot = True
-is_day = False
 
 if is_windy:
     to_draw += [
-        { "tile": f"{mode}_wind1", "pos": (7, 5) },
-        { "tile": f"{mode}_wind2", "pos": (7, 6) },
-        { "tile": f"{mode}_wind3", "pos": (7, 7) },
-        { "tile": f"{mode}_wind4", "pos": (7, 8) },
-        { "tile": f"{mode}_wind5", "pos": (7, 9) },
-        { "tile": f"{mode}_wind6", "pos": (7, 10) }
+        { "tile": f"{mode}_wind1", "pos": (7, 10) },
+        { "tile": f"{mode}_wind4", "pos": (13, 13) },
+        { "tile": f"{mode}_wind2", "pos": (23, 9) },
+        #{ "tile": f"{mode}_wind2", "pos": (7, 6) },
+        #{ "tile": f"{mode}_wind3", "pos": (7, 7) },
+        #{ "tile": f"{mode}_wind5", "pos": (7, 9) },
+        #{ "tile": f"{mode}_wind6", "pos": (7, 10) }
     ]
+    if is_windy > 3:
+        to_draw.append({ "tile": f"{mode}_wind3", "pos": (19, 12) })
+
+        if is_windy > 7:
+            to_draw.append({ "tile": f"{mode}_wind4", "pos": (7, 16) })
+            to_draw.append({ "tile": f"{mode}_wind6", "pos": (27, 15) })
+
+            if is_windy > 10:
+                to_draw.append({ "tile": f"{mode}_wind6", "pos": (21, 15) })
 
 to_draw.append({ "tile": f"{'sun'if is_day else 'moon'}{'4' if is_hot else '1'}", "pos": (27, 10) })
 
